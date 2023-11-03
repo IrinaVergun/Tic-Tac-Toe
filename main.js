ab10c95c04924b38935555b88df88b472df76678
Irina Vergun <antohka803@gmail.com>
2023-11-03 22:00:16 +0300

    добавила коментарии

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const winner = document.getElementById("winner");
-const cells = document.querySelectorAll(".cell");
-const resButton = document.getElementById("button");
-const nowGo = document.getElementById("nowGo");
+const winner = document.getElementById("winner"); //поле победителя
+const cells = document.querySelectorAll(".cell"); //каждая ячейка
+const resButton = document.getElementById("button"); //кнопка
+const nowGo = document.getElementById("nowGo"); //поле кто ходит
 const players = {
+  //постоянные в объекте чтобы не сложно их было достать
   x: "x",
   o: "o",
 };
-let currentPlayer = "";
-let isGameRunning = false;
-let arr = Array(9).fill("");
+let currentPlayer = ""; //текущему игроку
+let isGameRunning = false; //игра не идёт
+let arr = Array(9).fill(""); //пуустой массив чтобы потом его наполнить значениями
 const winLines = [
+  //выйгрышные линии
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -22,41 +24,50 @@ const winLines = [
 ];
 
 function init() {
+  //инициализация, привязывает клики по
   cells.forEach((cell) => {
-    cell.addEventListener("click", clickCell);
+    //
+    cell.addEventListener("click", clickCell); //ячейке
   });
-  resButton.addEventListener("click", restartGame);
+  resButton.addEventListener("click", restartGame); //по кнопке к функциям
 }
 
 function startGame() {
-  isGameRunning = true;
-  cells.forEach((cell) => (cell.textContent = ""));
-  winner.textContent = "";
-  currentPlayer = players.x;
-  nowGo.textContent = `Сейчас ходит : ${currentPlayer}`;
+  //начало игры
+  isGameRunning = true; //игра идёт
+  cells.forEach((cell) => (cell.textContent = "")); //перебираю фор ичем ккаждую ячейку и вставляю туда пустое значение
+  winner.textContent = ""; //также победителю
+  currentPlayer = players.x; //игр начинает х
+  nowGo.textContent = `Сейчас ходит : ${currentPlayer}`; // в полу кто ходит сообщаю чья очередь
 }
 function clickCell() {
+  //клик по кнопке
   if (!isGameRunning) {
-    return;
+    //если игра не запущена то вернуть
+    return; //
   } else if (this.textContent) {
-    return;
+    //если у ячейки есть значение вернуть это значение
+    return; //
   }
-  this.textContent = currentPlayer;
-  let cellIndex = this.dataset.cellIndex;
-  arr[cellIndex] = currentPlayer;
+  this.textContent = currentPlayer; //значение ячейки равняется текущему игроку
+  let cellIndex = this.dataset.cellIndex; //делаю переменную индкса соответсвующего яйчейки с индексом
+  arr[cellIndex] = currentPlayer; //привязываю мой массив  к этому индуксу и говорю что отныне они едино и равняюих к текущему игроку
   if (checkGameOver()) {
-    return finishGame();
+    //проверка закончена ли игра ли игра
+    return finishGame(); //конец игры
   }
   currentPlayer = currentPlayer === players.x ? players.o : players.x; //меняем текущего игрока на противоположного
   nowGo.textContent = `Сейчас ходит : ${currentPlayer}`; // в графу кто ходит записываем текущего игрока
 }
 
 function checkLine(line) {
-  const [a, b, c] = line;
+  //ходы выйгрыша
+  const [a, b, c] = line; //выйгрыш состоит из 3 ячеек
   const cellA = arr[a]; // выдергиваем значени из массива по индексу
-  const cellB = arr[b];
+  const cellB = arr[b]; //тоесть 3 значения а,б,с будут являться значениями в массиве
   const cellC = arr[c];
   if ([cellA, cellB, cellC].includes("")) {
+    //
     // проверяем что не одна из этих ячеек не содержит пустой линии
     return false;
   }
@@ -64,8 +75,10 @@ function checkLine(line) {
 }
 
 function checkGameOver() {
+  //
   //проверяем закончина ли игра
   for (const line of winLines) {
+    //линии в массиве выйгрыш линий
     if (checkLine(line)) {
       //если вернёт тру значит есть победить а тру он вернёт из фунции чек лайн
       winner.textContent = `${currentPlayer} Победил-_-`;
@@ -80,17 +93,20 @@ function checkGameOver() {
 }
 
 function finishGame() {
+  //финишь же поэтому обнуляем массив и кто ходит говорим что игра не идёт
   isGameRunning = false;
   nowGo.textContent = "";
   arr = Array(9).fill("");
 }
 
 function restartGame() {
+  //после начатия рестарта заканчивает игру и начинает новую
   finishGame();
   startGame();
 }
 
 window.addEventListener("load", () => {
+  //нужен виндоу чтобы скрипт смог прогрузиться
   init();
   startGame();
 });
